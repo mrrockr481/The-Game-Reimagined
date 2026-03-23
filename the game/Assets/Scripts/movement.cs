ebc0a74db3a7f46f64ed4853d298fdd6a095c681
mrrockr481 <mrrockr481@gmail.com>
2026-03-23 15:04:32 +0400

    fized movement

--- the game/Assets/Scripts/movement.cs
+++ the game/Assets/Scripts/movement.cs
@@ -15,17 +15,33 @@ public class movement : MonoBehaviour
         {
             rb.linearVelocity = new Vector3(0, 0, 10f); // move forward
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.S))
         {
             rb.linearVelocity = new Vector3(0, 0, -10f); // move forward
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.A))
         {
             rb.linearVelocity = new Vector3(-10f, 0, 0); // move forward
         }
         if (Input.GetKey(KeyCode.D))
         {
             rb.linearVelocity = new Vector3(10f, 0, 0); // move forward
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            rb.linearVelocity = new Vector3(0, 0, 0f); // move forward
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            rb.linearVelocity = new Vector3(0, 0, 0f); // move forward
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            rb.linearVelocity = new Vector3(0f, 0, 0); // move forward
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            rb.linearVelocity = new Vector3(0f, 0, 0); // move forward
         }
     }
 }
